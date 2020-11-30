3eab1a04055abbd11f58fa3189cba5e31603c4ef
Ying Huang <huangyingw@gmail.com>
2020-11-30 15:19:06 -0800

    n

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ cd "$SCRIPTPATH"
 
 
 NODE_OPTIONS=--max_old_space_size=5096 npm run build:dev --watch=false
-NODE_OPTIONS=--max_old_space_size=5096 npx ng run minds:server:production
-npm i
-NODE_OPTIONS=--max_old_space_size=5096 ENGINE_PORT=8080 npm run serve:dev
+# NODE_OPTIONS=--max_old_space_size=5096 npx ng run minds:server:production
+# npm i
+# NODE_OPTIONS=--max_old_space_size=5096 ENGINE_PORT=8080 npm run serve:dev
