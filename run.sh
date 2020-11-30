5a9e27ff71fc663ed0defaf908831c4927229aea
Ying Huang <huangyingw@gmail.com>
2020-11-30 15:05:32 -0800

    n

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+
+NODE_OPTIONS=--max_old_space_size=5096 npm run build:dev --watch=false
+NODE_OPTIONS=--max_old_space_size=5096 npx ng run minds:server:production
+npm i
+NODE_OPTIONS=--max_old_space_size=5096 ENGINE_PORT=8080 npm run serve:dev
