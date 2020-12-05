d0ef0747fad00bb79557f31718dfdfb79dd41439
Ying Huang <huangyingw@gmail.com>
2020-12-04 21:10:52 -0800

    n

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-
+# rm -fr ./node_modules/*
+# rm -fr ./dist/*
 npm i && \
     NODE_OPTIONS=--max_old_space_size=5096 npm run build:dev --watch=false
     # NODE_OPTIONS=--max_old_space_size=5096 npx ng run minds:server:production
