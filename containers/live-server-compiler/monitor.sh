8a13b71ed8cec763b3f8b37d4853a9e64bdd9124
Ying Huang <huangyingw@gmail.com>
2020-12-12 20:39:12 -0800

    n

--- containers/live-server-compiler/monitor.sh
+++ containers/live-server-compiler/monitor.sh
@@ -2,4 +2,6 @@
 set -e
 
 cd /var/www/Minds/front
-npx nodemon --delay 3 --watch server.ts --watch dist/en/ --ext js,css,jpg,png,svg,mp4,webp,webm  --exec "/usr/bin/env sh" /build.sh
+# npx nodemon --delay 3 --watch server.ts --watch dist/en/ --ext js,css,jpg,png,svg,mp4,webp,webm  --exec "/usr/bin/env sh" /build.sh
+npm rebuild node-sass
+NODE_OPTIONS=--max_old_space_size=5096 npm run build:dev --watch=false
