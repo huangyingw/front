4d488382810383a8c03bd73bb4111000fd05faf7
Ying Huang <huangyingw@gmail.com>
2020-12-22 11:02:02 -0800

    n

--- run.sh
+++ run.sh
@@ -3,13 +3,8 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-npm i \
-    crypto-js \
-    http \
-    -S
+rm -fr ./node_modules/*
+rm ./package-lock.json
 
-npm rebuild node-sass
 npm i && \
     NODE_OPTIONS=--max_old_space_size=5096 npm run build:dev --watch=false
-    # NODE_OPTIONS=--max_old_space_size=5096 npx ng run minds:server:production
-    # NODE_OPTIONS=--max_old_space_size=5096 ENGINE_PORT=8080 npm run serve:dev
