0463236691d846dc37ff4255915965f755f582b6
Ying Huang <huangyingw@gmail.com>
2020-12-24 21:44:22 -0800

    n

---
+++ build-front.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+rm -fr ./node_modules/*
+git co ./package-lock.json
+
+npm install && \
+    NODE_OPTIONS=--max_old_space_size=5096 npm run build:dev -- --watch=false && \
+    NODE_OPTIONS=--max_old_space_size=5096 npx ng run minds:server:production
