e1b53c11ef73cc3c370d780830fad82eb24eaf49
Ying Huang <huangyingw@gmail.com>
2020-12-24 16:03:07 -0800

    n

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 rm -fr ./node_modules/*
-rm ./package-lock.json
+git co ./package-lock.json
 
 npm i && \
     NODE_OPTIONS=--max_old_space_size=5096 npm run build:dev --watch=false
