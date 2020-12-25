a186b503405810290c46a18c203e41e77c8370ad
Ying Huang <huangyingw@gmail.com>
2020-12-24 16:02:16 -0800

    n

--- containers/live-server-compiler/monitor.sh
+++ containers/live-server-compiler/monitor.sh
@@ -3,6 +3,8 @@ set -e
 
 cd /var/www/Minds/front/
 rm -fr node_modules/* 
+rm package-lock.json
 
+npm i yargs --save
 npm i && \
     NODE_OPTIONS=--max_old_space_size=5096 npm run build:dev --watch=false
